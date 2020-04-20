07c9601f115724564b7345f3ac9831e0b58e5c18
Nathan Rajlich <nathan@tootallnate.net>
2020-04-20 15:23:14 -0700

    Add `once.spread()` function
    Returns an array of all parameters provided to `emit()`.
    
    Useful for events like child process "exit", where is has two parameters:
    
    ```typescript
    const [ code, signal ] = await once.spread<[number, string | null]>(child, 'exit');
    ```

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,48 @@ function once<T>(
 	emitter: EventEmitter,
 	name: string
 ): once.CancellablePromise<T> {
-	let c: once.CancelFunction | null = null;
-	const p = new Promise<T>((resolve, reject) => {
-		function cancel() {
-			emitter.removeListener(name, onEvent);
-			emitter.removeListener('error', onError);
-		}
-		function onEvent(arg: T) {
-			cancel();
-			resolve(arg);
-		}
-		function onError(err: Error) {
-			cancel();
-			reject(err);
-		}
-		c = cancel;
-		emitter.on(name, onEvent);
-		emitter.on('error', onError);
-	}) as once.CancellablePromise<T>;
-	if (!c) {
-		throw new TypeError('Could not get `cancel()` function');
-	}
-	p.cancel = c;
-	return p;
+	const o = once.spread<[T]>(emitter, name);
+	const r = o.then((args: [T]) => args[0]) as once.CancellablePromise<T>;
+	r.cancel = o.cancel;
+	return r;
 }
 
 namespace once {
 	export interface CancelFunction {
 		(): void;
 	}
+
 	export interface CancellablePromise<T> extends Promise<T> {
 		cancel: CancelFunction;
+	}
+
+	export function spread<T extends any[]>(
+		emitter: EventEmitter,
+		name: string
+	): once.CancellablePromise<T> {
+		let c: once.CancelFunction | null = null;
+		const p = new Promise<T>((resolve, reject) => {
+			function cancel() {
+				emitter.removeListener(name, onEvent);
+				emitter.removeListener('error', onError);
+			}
+			function onEvent(...args: any[]) {
+				cancel();
+				resolve(args as T);
+			}
+			function onError(err: Error) {
+				cancel();
+				reject(err);
+			}
+			c = cancel;
+			emitter.on(name, onEvent);
+			emitter.on('error', onError);
+		}) as once.CancellablePromise<T>;
+		if (!c) {
+			throw new TypeError('Could not get `cancel()` function');
+		}
+		p.cancel = c;
+		return p;
 	}
 }
 
