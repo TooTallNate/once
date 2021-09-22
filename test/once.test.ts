7114d356c88e2c7ca779daf34d4334603695a664
Nathan Rajlich <n@n8.io>
2021-09-21 23:27:27 -0700

    Add a couple more tests

--- test/once.test.ts
+++ test/once.test.ts
@@ -3,31 +3,54 @@ import { EventEmitter } from 'events';
 import once from '../src';
 
 describe('once()', () => {
-    it('should work with vanilla EventEmitter', async () => {
-        const emitter = new EventEmitter();
-        const promise = once(emitter, 'foo');
-        emitter.emit('foo', 'bar');
-        const [ foo ] = await promise;
-        expect(foo).toEqual('bar');
-    });
+	it('should work with vanilla EventEmitter', async () => {
+		const emitter = new EventEmitter();
+		const promise = once(emitter, 'foo');
+		emitter.emit('foo', 'bar');
+		const [foo] = await promise;
+		expect(foo).toEqual('bar');
+	});
 
-    it('should work with interface extending from EventEmitter with overload', async () => {
-        interface TestEmitter extends EventEmitter {
-            on(name: 'foo', listener: (a: string, b: number) => void): this;
-        }
+	it('should reject Promise upon "error" event', async () => {
+		let err: Error | null = null;
+		const emitter = new EventEmitter();
+		const promise = once(emitter, 'foo').catch((_err) => {
+			err = _err;
+		});
+		emitter.emit('error', new Error('test'));
+		await promise;
+		expect(err!.message).toEqual('test');
+	});
 
-        const emitter: TestEmitter = new EventEmitter();
-        const promise = once(emitter, 'foo');
-        emitter.emit('foo', 'bar', 4);
-        const [ a, b ] = await promise;
-        expect(a).toEqual('bar');
-        expect(b).toEqual(4);
-    });
+	it('should work with interface extending EventEmitter with overload', async () => {
+		interface TestEmitter extends EventEmitter {
+			on(name: 'foo', listener: (a: string, b: number) => void): this;
+		}
 
-    it('should work with ChildProcess "exit" event', async () => {
-        const child = spawn('echo', ['hi']);
-        const [ code, signal ] = await once(child, 'exit');
-        expect(code).toEqual(0)
-        expect(signal).toBeNull();
-    });
+		const emitter: TestEmitter = new EventEmitter();
+		const promise = once(emitter, 'foo');
+		emitter.emit('foo', 'bar', 4);
+		const [a, b] = await promise;
+		expect(a).toEqual('bar');
+		expect(b).toEqual(4);
+	});
+
+	it('should allow casting from an `any` param', async () => {
+		interface TestEmitter extends EventEmitter {
+			on(name: 'foo', listener: (a: any, b: number) => void): this;
+		}
+
+		const emitter: TestEmitter = new EventEmitter();
+		const promise: Promise<[string, unknown]> = once(emitter, 'foo');
+		emitter.emit('foo', 'bar', 4);
+		const [a] = await promise;
+		expect(a).toEqual('bar');
+	});
+
+	it('should work with ChildProcess "exit" event', async () => {
+		const child = spawn('echo', ['hi']);
+		const [code, signal] = await once(child, 'exit');
+		expect(code).toEqual(0);
+		expect(signal).toBeNull();
+	});
 });
